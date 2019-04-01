55836b8601391b40e1a21a4bc3d2e67a3da191fe
Vladokas <marmuh13@mail.ru>
2019-04-01 10:36:24 +0300

    commitnumber 2

--- практика1/практика1/Program.cs
+++ практика1/практика1/Program.cs
@@ -6,4 +6,33 @@ using System.Threading.Tasks;
 
 namespace практика1
 {
- 
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Выберите нужное дейсвие:");
+                Console.WriteLine("2. Регистрация оружия");
+                Console.WriteLine("0 выход");
+
+                int c = Int32.Parse(Console.ReadLine());
+                switch (c)
+                {
+
+                    case 2:
+                        examen lol = new examen();
+                        lol.GetInfo();
+                        break;
+                    case 0:
+                        flag = false;
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+
+
+
